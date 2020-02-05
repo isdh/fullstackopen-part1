f9b215d7d24a395adf88d43422cec73c47f49ca9
isdh <hideorseekyes+github@gmail.com>
2020-02-05 15:45:16 +0900

    implements component helper function

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Hello = props => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  };
   return (
     <div>
       <p>
         Hello {props.name}, you are {props.age} years old
       </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   );
 };
