429f5c5455c9dd48b983b69017cbc7c60426aa1b
isdh <hideorseekyes+github@gmail.com>
2020-02-05 16:38:32 +0900

    currying setToValue function

--- src/index.js
+++ src/index.js
@@ -14,22 +14,17 @@ const Hello = ({ name, age }) => {
   );
 };
 
-/**
- * 状態変更関数setCounterが呼び出されると、
- * Reactはコンポーネントを再レンダリングする。
- * つまり、コンポーネント関数の関数本体が再実行されます。
- * @param {*} props
- */
 const App = props => {
   const [counter, setCounter] = useState(0);
 
-  const setToValue = value => setCounter(value);
+  // double arrow functoin
+  const setToValue = value => () => setCounter(value);
 
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={() => setToValue(counter + 1)}>plus</button>
-      <button onClick={() => setToValue(0)}>reset</button>
+      <button onClick={setToValue(counter + 1)}>plus</button>
+      <button onClick={setToValue(0)}>reset</button>
     </div>
   );
 };
