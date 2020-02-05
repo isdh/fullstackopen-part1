9379c9a920570abef6ecd5e5cdf5cc7a7b455e40
isdh <hideorseekyes+github@gmail.com>
2020-02-05 17:21:21 +0900

    comment out unuse functions

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
+// const Hello = ({ name, age }) => {
+//   const bornYear = () => new Date().getFullYear() - age;
 
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
+//       <p>So you were probably born in {bornYear()}</p>
+//     </div>
+//   );
+// };
 
-const Display = ({ counter }) => {
-  return <div>{counter}</div>;
-};
+// const Display = ({ counter }) => {
+//   return <div>{counter}</div>;
+// };
 
-const Button = props => {
-  return <button onClick={props.onClick}>{props.text}</button>;
-};
+// const Button = props => {
+//   return <button onClick={props.onClick}>{props.text}</button>;
+// };
 
 const App = props => {
   const [counter, setCounter] = useState(0);
